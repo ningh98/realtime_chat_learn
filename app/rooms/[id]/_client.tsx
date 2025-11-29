9cd04ed74ec2af781dc4ab4247b580b569f4d74b
ningh98 <ninghuicai@Ninghuis-MBP.hsd1.ca.comcast.net>
2025-11-28 20:37:09 -0800

    send and display message in chat not realtime yet

--- app/rooms/[id]/_client.tsx
+++ app/rooms/[id]/_client.tsx
@@ -1,61 +1,53 @@
-"use client"
+"use client";
 
-import { ChatInput } from "@/components/chat-input"
-import { Message } from "@/services/supabase/actions/messages"
+import { ChatInput } from "@/components/chat-input";
+import { ChatMessage } from "@/components/chat-message";
+import { Message } from "@/services/supabase/actions/messages";
 
 export function RoomClient({
-    room,
-    user,
-    messages
+  room,
+  user,
+  messages,
 }: {
-    room: {
-        id: string,
-        name: string,
-    }
-    user:{
-        id: string,
-        name: string,
-        image_url: string | null
-    }
-    messages: Message[]
-}){
-    return (
-        <div className="container mx-auto h-screen-with-header border border-y-0 flex flex-col">
-            <div className="flex items-center justify-between gap-2">
-                <div className="p-4 border-b">
-                    <h1 className="text-2xl font-bold">{room.name}</h1>
-                    {/* TODO make real data */}
-                    <p className="text-muted-foreground text-sm">
-                        0 users online
-                    </p>
-                    <InviteUseerModal roomId={room.id} />
-                </div>
-            </div>
-            <div className="grow overflow-y-auto flex flex-col-reverse"
-                style={{
-                    scrollbarWidth: "thin",
-                    scrollbarColor: "var(--border) transparent"
-                }}
-            >
-                <div>
-                    {messages.map((message)=> (
-                        <ChatMessage key={message.id} {...message} />
-                    ))}
-                </div>
-                <ChatInput roomId={room.id}/>
-            </div>
-        </div>
-    )
-}
-
-function InviteUseerModal({
-    roomId
-}: {
-    roomId: string
+  room: {
+    id: string;
+    name: string;
+  };
+  user: {
+    id: string;
+    name: string;
+    image_url: string | null;
+  };
+  messages: Message[];
 }) {
-    return null
+  return (
+    <div className="container mx-auto h-screen-with-header border border-y-0 flex flex-col">
+      <div className="flex items-center justify-between gap-2">
+        <div className="p-4 border-b">
+          <h1 className="text-2xl font-bold">{room.name}</h1>
+          {/* TODO make real data */}
+          <p className="text-muted-foreground text-sm">0 users online</p>
+          <InviteUseerModal roomId={room.id} />
+        </div>
+      </div>
+      <div
+        className="grow overflow-y-auto flex flex-col-reverse"
+        style={{
+          scrollbarWidth: "thin",
+          scrollbarColor: "var(--border) transparent",
+        }}
+      >
+        <div>
+          {messages.toReversed().map((message) => (
+            <ChatMessage key={message.id} {...message} />
+          ))}
+        </div>
+        <ChatInput roomId={room.id} />
+      </div>
+    </div>
+  );
 }
 
-function ChatMessage(){
-    return null
+function InviteUseerModal({ roomId }: { roomId: string }) {
+  return null;
 }
--- components/chat-input.tsx
+++ components/chat-input.tsx
@@ -1,3 +1,57 @@
-export function ChatInput({ roomId}:{ roomId: string }) {
-    return null
+"use client";
+
+import { SendIcon } from "lucide-react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupTextarea,
+} from "./ui/input-group";
+import { FormEvent, useState } from "react";
+import { toast } from "sonner";
+import { sendMessage } from "@/services/supabase/actions/messages";
+
+export function ChatInput({ roomId }: { roomId: string }) {
+  const [message, setMessage] = useState("");
+
+  async function handleSubmit(e?: FormEvent) {
+    e?.preventDefault();
+    const text = message.trim();
+    if (!text) return;
+
+    setMessage("");
+    const result = await sendMessage({ text: message, roomId });
+    if (result.error) {
+      toast.error(result.message);
+    } else {
+    }
+  }
+  return (
+    <form className="p-3">
+      <InputGroup>
+        <InputGroupTextarea
+          placeholder="Type your message..."
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="field-sizing-content min-h-auto"
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+              handleSubmit();
+            }
+          }}
+        />
+        <InputGroupAddon align="inline-end">
+          <InputGroupButton
+            type="submit"
+            aria-label="Send"
+            title="Send"
+            size="icon-sm"
+          >
+            <SendIcon />
+          </InputGroupButton>
+        </InputGroupAddon>
+      </InputGroup>
+    </form>
+  );
 }
